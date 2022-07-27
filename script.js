b11152546d5b28f6d77063b6ee0003da2e3b8b71
Tolibjon <tolibjohn@gmail.com>
2022-07-27 19:00:58 +0500

    Add rotation buttons

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const fileBtn = document.querySelector("#file-btn");
 const filterName = document.querySelector("#filter-name");
 const range = document.querySelector("#range");
 const rangeValue = document.querySelector("#range-value");
-const resetButton = document.querySelector("#reset-btn");
+const resetButton = document.querySelector("#reset-button");
 
 const filters = {
   brightness:50, 
@@ -28,7 +28,6 @@ filterButtons.forEach(button=>{
     document.querySelector('.filters .active-btn')?.classList.remove('active-btn')
     button.classList.add('active-btn');
     filterName.textContent = button.textContent;
-    console.log(button.name);
     range.value = filters[button.name];
     rangeValue.textContent = filters[button.name]+'%';
   })
@@ -38,6 +37,19 @@ range.addEventListener('change', (e)=>{
   const activeButton = document.querySelector('.filters .active-btn').name
   filters[activeButton] = e.target.value;
   rangeValue.textContent = filters[activeButton]+'%';
-  console.log(filters)
 });
 
+resetButton.addEventListener('click', ()=>{
+  filters.brightness=50; 
+  filters.saturation=50;
+  filters.inversion=0;
+  filters.grayscale=0;
+  document.querySelector('[name="brightness"]').click()
+});
+
+const rotationButtons = document.querySelectorAll('.rotate-buttons button');
+rotationButtons.forEach(button=>{
+    button.addEventListener('click', (e)=>{
+    console.log(e.target.name)
+  })
+});
